b9b02c39c5e5383e6928e21bd5d765248ef29705
Lele2405 <lethokuhlezwane1@gmail.com>
2023-09-14 08:45:48 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase characters
- * @c: function paraameter
- * Return: 1 on success 0 on failure
+ * _isupper - checks if a character is uppercase
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
