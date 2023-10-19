27653a0fce43dc5027399aa3079c8041307d14d1
Lele2405 <lethokuhlezwane1@gmail.com>
2023-10-19 07:48:26 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -1,49 +1,26 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
+int main(void) {
+	list_t *head = NULL;head = add_node_end(&head, "Anne");
+	
+	add_node_end(&head, "Colton");
+	add_node_end(&head, "Corbin");
+	add_node_end(&head, "Daniel");
+	add_node_end(&head, "Danton");
+	add_node_end(&head, "David");
+	add_node_end(&head, "Gary");
+	add_node_end(&head, "Holden");
+	add_node_end(&head, "Ian");
+	add_node_end(&head, "Ian");
 
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
-}#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
+	printf("Initial list:\n");
+	print_list(head);
+	
+	/* Free the list */
+	free_list(head);
+	head = NULL;
+	
+	return 0;
 }
